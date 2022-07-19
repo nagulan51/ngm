c83aaf9c7d537c4cfc04532e03935c0977702d52
Nagulan <nagulan77fr@icloud.com>
2022-07-20 00:46:04 +0200

    ddd

--- src/model/menu.jsx
+++ src/model/menu.jsx
@@ -7,7 +7,7 @@ return (
   <>
   <ul className="menu">
     {logo ? <h1 className="menu_logo">Nagulan</h1>: undefined}
-    {menu_list.map((el,index) => ( <li id={id-index} key={id-index} className={index === active_menu ? index +' active': index}>{el}</li> ))}  
+    {menu_list.map((el,index) => ( <li id={id+index} key={id+index} className={index === active_menu ? index +' active': index}>{el}</li> ))}  
   </ul>
   </>
   );
