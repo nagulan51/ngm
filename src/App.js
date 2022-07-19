c7be46b5019f307cd52c88b49cab3aae0a1def36
Nagulan <nagulan77fr@icloud.com>
2022-07-20 00:37:49 +0200

    ddd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useId} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Menu from './model/menu'
+function App() 
+{
+  //var [button1, setButton1] = useState(0);
+  var id = useId();
+  return (<>
+    {/* <button onMouseOver={() => setButton1(button1+1)}>{button1}</button> */}
+    <Menu id={id} key={id} logo={1} active_menu={0} menu_list={['Accueil','Projets','Qui je-suis ?','Contact']} ></Menu>
+  </>);
 }
 
 export default App;
---
+++ src/model/menu.jsx
@@ -0,0 +1,16 @@
+import { useId } from "react";
+
+function Menu({menu_list,active_menu,logo}) 
+{
+const id = useId();
+return (
+  <>
+  <ul className="menu">
+    {logo ? <h1 className="menu_logo">Nagulan</h1>: undefined}
+    {menu_list.map((el,index) => ( <li id={id-index} key={id-index} className={index === active_menu ? index +' active': index}>{el}</li> ))}  
+  </ul>
+  </>
+  );
+}
+
+export default Menu;
